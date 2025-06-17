3a7aa9410d10974ccf3e890d63cd49dd5c408057
ADAMalves <adam.alves@ynov.com>
2025-06-17 15:06:30 +0200

    modif server

--- server.js
+++ server.js
@@ -3,26 +3,40 @@ const admin = require('firebase-admin');
 const bodyParser = require('body-parser');
 const path = require('path');
 
-const serviceAccount = require('./jeurole-16332-firebase-adminsdk-fbsvc-3a01cf3d22.json');
-
+// Configuration Firebase à partir des variables d'environnement
 admin.initializeApp({
-  credential: admin.credential.cert(serviceAccount),
+  credential: admin.credential.cert({
+    type: process.env.FIREBASE_TYPE,
+    project_id: process.env.FIREBASE_PROJECT_ID,
+    private_key_id: process.env.FIREBASE_PRIVATE_KEY_ID,
+    private_key: process.env.FIREBASE_PRIVATE_KEY.replace(/\\n/g, '\n'),
+    client_email: process.env.FIREBASE_CLIENT_EMAIL,
+    client_id: process.env.FIREBASE_CLIENT_ID,
+    auth_uri: process.env.FIREBASE_AUTH_URI,
+    token_uri: process.env.FIREBASE_TOKEN_URI,
+    auth_provider_x509_cert_url: process.env.FIREBASE_AUTH_PROVIDER_CERT_URL,
+    client_x509_cert_url: process.env.FIREBASE_CLIENT_CERT_URL,
+    universe_domain: process.env.FIREBASE_UNIVERSE_DOMAIN,
+  }),
   databaseURL: 'https://jeurole-16332.firebaseio.com',
 });
 
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
+// Middlewares
 app.use(bodyParser.json());
 app.use(express.static('public'));
-app.use('/assets', express.static(__dirname + '/assets'));
-app.use('/css', express.static(__dirname + '/public/css'));
-app.use('/js', express.static(__dirname + '/public/js'));
+app.use('/assets', express.static(path.join(__dirname, 'assets')));
+app.use('/css', express.static(path.join(__dirname, 'public/css')));
+app.use('/js', express.static(path.join(__dirname, 'public/js')));
 
+// Routes
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, '/public/login.html')));
 app.get('/register', (req, res) => res.sendFile(path.join(__dirname, '/public/register.html')));
 app.get('/home', (req, res) => res.sendFile(path.join(__dirname, '/public/home.html')));
 
+// Lancement du serveur
 app.listen(port, () => {
   console.log(`Serveur lancé sur http://localhost:${port}`);
 });
